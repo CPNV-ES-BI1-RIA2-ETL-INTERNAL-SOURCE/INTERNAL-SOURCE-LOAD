880434297d64d3a506e14e8a611734b32525b0b2
NoahDelgado <noah.delgado1@eduvaud.ch>
2025-03-21 19:29:07 +0100

    refactor: Simplify SqlInsertGeneratorTests by using test data and improving query validation

--- INTERNAL_SOURCE_LOAD_TEST/SqlInsertGeneratorTests.cs
+++ INTERNAL_SOURCE_LOAD_TEST/SqlInsertGeneratorTests.cs
@@ -6,69 +6,44 @@ using INTERNAL_SOURCE_LOAD.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework.Legacy;
-
+using INTERNAL_SOURCE_LOAD_TEST.TestData;
+using static INTERNAL_SOURCE_LOAD_TEST.TestData.SqlTestData;
 
 namespace INTERNAL_SOURCE_LOAD_TEST
 {
     [TestFixture]
     public class SqlInsertGeneratorTests
     {
-        public class SimpleModel
-        {
-            public int Id { get; set; }
-            public required string Name { get; set; }
-        }
-
-        public class ComplexModel
-        {
-            public int Id { get; set; }
-            public required string Name { get; set; }
-            public List<SimpleModel>? Items { get; set; }
-        }
-
         [Test]
         public void GenerateInsertQueries_GivenValidSimpleObject_ReturnsExpectedQuery()
         {
             // Given
-            string valideQuery = "INSERT INTO SimpleModel (Id, Name) VALUES (1, 'Test');";
-
-            var simpleModel = new SimpleModel { Id = 1, Name = "Test" };
+            string validQuery = GetValidInsertQuery();
+            var simpleModel = GetSimpleModel();
 
             // When
             var result = SqlInsertGenerator.GenerateInsertQueries("SimpleTable", simpleModel);
 
             // Then
             Assert.That(result.Count, Is.EqualTo(1));
-            Assert.That(valideQuery, Is.EqualTo(result[0].Query));
+            Assert.That(validQuery, Is.EqualTo(result[0].Query));
         }
 
         [Test]
         public void GenerateInsertQueries_GivenNestedObject_ReturnsExpectedQueries()
         {
             // Given
-            string valideQuery1 = "INSERT INTO ComplexModel (Id, Name) VALUES (1, 'Parent');";
-            string valideQuery2 = "INSERT INTO SimpleModel (Id, Name) VALUES (2, 'Child1');";
-            string valideQuery3 = "INSERT INTO SimpleModel (Id, Name) VALUES (3, 'Child2');";
-            var complexModel = new ComplexModel
-            {
-                Id = 1,
-                Name = "Parent",
-                Items = new List<SimpleModel>
-                    {
-                        new SimpleModel { Id = 2, Name = "Child1" },
-                        new SimpleModel { Id = 3, Name = "Child2" }
-                    }
-            };
+            var (validQuery1, validQuery2, validQuery3) = GetNestedObjectQueries();
+            var complexModel = GetComplexModel();
 
             // When
             var result = SqlInsertGenerator.GenerateInsertQueries("ComplexModel", complexModel);
 
             // Then
             Assert.That(result.Count, Is.EqualTo(3));
-            Assert.That(valideQuery1, Is.EqualTo(result[2].Query));
-            Assert.That(valideQuery2, Is.EqualTo(result[0].Query));
-            Assert.That(valideQuery3, Is.EqualTo(result[1].Query));
-
+            Assert.That(validQuery1, Is.EqualTo(result[2].Query));
+            Assert.That(validQuery2, Is.EqualTo(result[0].Query));
+            Assert.That(validQuery3, Is.EqualTo(result[1].Query));
         }
 
         [Test]
@@ -89,10 +64,7 @@ namespace INTERNAL_SOURCE_LOAD_TEST
         public void GenerateInsertQueries_GivenTopLevelCollection_ThrowsInvalidOperationException()
         {
             // Given
-            var simpleModels = new List<SimpleModel>
-                {
-                    new SimpleModel { Id = 1, Name = "Test" }
-                };
+            var simpleModels = GetSimpleModelList();
 
             // When & Then
             var exception = Assert.Throws<InvalidOperationException>(() =>
@@ -106,29 +78,14 @@ namespace INTERNAL_SOURCE_LOAD_TEST
         public void GenerateUpdateForeignKeys_GivenDepartureWithTrain_ReturnsCorrectUpdateQuery()
         {
             // Given
-            var train = new Train("ICE", "DB123");
-            var departure = new Departure(
-                DepartureStationName: "Berlin",
-                DestinationStationName: "Munich",
-                ViaStationNames: new List<string> { "Frankfurt" },
-                DepartureTime: DateTime.Now,
-                Train: train,
-                Platform: "1"
-            );
+            var trainStation = TrainStationTestData.GetComplexTrainStation();
+            var departure = trainStation.Departures[0];
+            var modelIds = TrainStationTestData.GetModelIds(trainStation);
 
-            var modelIds = new Dictionary<object, long>
-                {
-                    { train, 42 },      // Train got ID 42
-                    { departure, 123 }   // Departure got ID 123
-                };
-
-            string expectedQuery = "UPDATE Departures SET TrainID = 42 WHERE Id = 123";
+            string expectedQuery = GetValidUpdateQuery();
 
             // When
             var result = SqlInsertGenerator.GenerateUpdateForeignKeysQueries(departure, modelIds);
-            Console.WriteLine(result);
-            //run the query to see if it works
-
 
             // Then
             Assert.That(result, Is.Not.Empty);
@@ -139,100 +96,30 @@ namespace INTERNAL_SOURCE_LOAD_TEST
         public void GenerateUpdateForeignKeys_GivenMultipleDeparturesWithDifferentTrains_ReturnsCorrectUpdateQueries()
         {
             // Given
-            var train1 = new Train("ICE", "DB123");
-            var train2 = new Train("TGV", "FR456");
-            var train3 = new Train("ICE", "DB789");
-
-            var departure1 = new Departure(
-                DepartureStationName: "Berlin",
-                DestinationStationName: "Munich",
-                ViaStationNames: new List<string> { "Frankfurt" },
-                DepartureTime: DateTime.Now,
-                Train: train1,
-                Platform: "1"
-            );
-
-            var departure2 = new Departure(
-                DepartureStationName: "Paris",
-                DestinationStationName: "Lyon",
-                ViaStationNames: new List<string> { "Dijon" },
-                DepartureTime: DateTime.Now,
-                Train: train2,
-                Platform: "2"
-            );
-            var departure3 = new Departure(
-                DepartureStationName: "Berlin",
-                DestinationStationName: "Paris",
-                ViaStationNames: new List<string> { "Frankfurt" },
-                DepartureTime: DateTime.Now,
-                Train: train3,
-                Platform: "1"
-            );
-
-            var modelIds = new Dictionary<object, long>
-                {
-                    { train1, 42 },     // First train ID
-                    { train2, 43 },     // Second train ID
-                    { train3, 44 },     // Third train ID
-                    { departure1, 123 }, // First departure ID
-                    { departure2, 124 }, // Second departure ID
-                    { departure3, 125 }  // Third departure ID
-                };
-
-            // Expected queries for both departures
-            var expectedQueries = new List<string>
-                {
-                    "UPDATE Departures SET TrainID = 42 WHERE Id = 123",
-                    "UPDATE Departures SET TrainID = 43 WHERE Id = 124",
-                    "UPDATE Departures SET TrainID = 44 WHERE Id = 125"
-                };
+            var trainStation = TrainStationTestData.GetComplexTrainStation();
+            var modelIds = TrainStationTestData.GetModelIds(trainStation);
+            var expectedQueries = GetMultipleUpdateQueries();
 
             // When
-            var result1 = SqlInsertGenerator.GenerateUpdateForeignKeysQueries(departure1, modelIds);
-            var result2 = SqlInsertGenerator.GenerateUpdateForeignKeysQueries(departure2, modelIds);
-            var result3 = SqlInsertGenerator.GenerateUpdateForeignKeysQueries(departure3, modelIds);
+            var results = trainStation.Departures.Select(d =>
+                SqlInsertGenerator.GenerateUpdateForeignKeysQueries(d, modelIds)[0]).ToList();
 
             // Then
-            Assert.That(result1.Count + result2.Count + result3.Count, Is.EqualTo(3), "Should generate 3 update queries");
-            Assert.That(result1[0], Is.EqualTo(expectedQueries[0]), "First departure should reference train1");
-            Assert.That(result2[0], Is.EqualTo(expectedQueries[1]), "Second departure should reference train2");
-            Assert.That(result3[0], Is.EqualTo(expectedQueries[2]), "Third departure should reference train3");
-
-            // Additional verification that the queries are different
-            Assert.That(result1[0], Is.Not.EqualTo(result2[0]), "Update queries should be different for different trains");
-            Assert.That(result2[0], Is.Not.EqualTo(result3[0]), "Update queries should be different for different trains");
-            Assert.That(result1[0], Is.Not.EqualTo(result3[0]), "Update queries should be different for different trains");
+            Assert.That(results.Count, Is.EqualTo(3), "Should generate 3 update queries");
+            for (int i = 0; i < results.Count; i++)
+            {
+                Assert.That(results[i], Is.EqualTo(expectedQueries[i]),
+                    $"Departure {i + 1} should have correct foreign key update query");
+            }
         }
 
         [Test]
         public void GenerateUpdateForeignKeys_GivenDepartureWithTrainStation_ReturnsCorrectUpdateQueries()
         {
             // Given
-            var train = new Train("ICE", "DB123");
-            var departure = new Departure(
-                DepartureStationName: "Berlin",
-                DestinationStationName: "Munich",
-                ViaStationNames: new List<string> { "Frankfurt" },
-                DepartureTime: DateTime.Now,
-                Train: train,
-                Platform: "1",
-                TrainStationID: null  // This should be updated by the foreign key update
-            );
-            var trainStation = new TrainStation("Berlin Hbf", Departures: new List<Departure> { departure });
-
-
-            var modelIds = new Dictionary<object, long>
-                {
-                    { train, 42 },         // Train ID
-                    { trainStation, 55 },   // TrainStation ID
-                    { departure, 123 }      // Departure ID
-                };
-
-            var expectedQueries = new List<string>
-                {
-                    "UPDATE Departures SET TrainID = 42 WHERE Id = 123",
-                    "UPDATE Departures SET TrainStationID = 55 WHERE Id = 123"
-                };
+            var (trainStation, train, departure) = TrainStationTestData.GetSimpleTrainStation();
+            var modelIds = TrainStationTestData.GetSimpleModelIds(train, departure, trainStation);
+            var expectedQueries = GetTrainStationUpdateQueries();
 
             // When
             var result = SqlInsertGenerator.GenerateUpdateForeignKeysQueries(departure, modelIds);
